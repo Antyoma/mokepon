e90a67a6f5c6eca746c276c70f9894eb9c09ee7c
Anthony Matheus <antyoma33@gmail.com>
2023-07-11 16:18:29 -0500

    ocultando secciones

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -4,6 +4,12 @@ let vidasJugador = 3;
 let vidasEnemigo = 3;
 
 function iniciarJuego(){
+    let sectionSeleccionarAtaque = document.getElementById('Seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'none';
+
+    let sectionReiniciar = document.getElementById('Reiniciar')
+    sectionReiniciar.style.display = 'none';
+
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 
@@ -19,6 +25,12 @@ function iniciarJuego(){
 }
 
 function seleccionarMascotaJugador() {
+    let sectionSeleccionarMascota = document.getElementById('Seleccionar-mascota')
+    sectionSeleccionarMascota.style.display = 'none';
+
+    let sectionSeleccionarAtaque = document.getElementById('Seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'block';
+
     let inputHipodoge = document.getElementById('hipodoge');
     let inputCapipepo = document.getElementById('capipepo');
     let inputRatigueya = document.getElementById('ratigueya');
@@ -135,7 +147,10 @@ function crearMensajeFinal(resultadoFinal){
     botonAgua.disabled = true
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.disabled = true
-    
+
+    let sectionReiniciar = document.getElementById('Reiniciar')
+    sectionReiniciar.style.display = 'block';
+
 }
 
 function reiniciarJuego(){
